0c456204064a95e59d194f8c57c916953593ceb2
stephanieeley <58796498+stephanieeley@users.noreply.github.com>
2019-12-19 18:58:23 -0700

    added ch assignments

---
+++ chapter 1/Exercise1-5.java
@@ -0,0 +1,8 @@
+//Stephanie Eley 12/18/2019
+
+class ComputExpression {
+	public static void main(String[] args) {
+		System.out.print("(9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5) = " );
+		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)); 
+	}
+}
---
+++ chapter 1/Exercise1-9.java
@@ -0,0 +1,8 @@
+//stephanie eley 12/18/2019
+
+class FindArea {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9);
+		System.out.println(2 * (4.5 + 7.9));
+	}
+}
