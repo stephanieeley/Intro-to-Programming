7dbf258a5da3a5db5a6cf89a6a07a267710134db
stephanieeley <58796498+stephanieeley@users.noreply.github.com>
2020-01-15 19:26:52 -0700

    Add files via upload

---
+++ Chapter 5/Exercise5_11.java
@@ -0,0 +1,20 @@
+//Stephanie Eley 01/15/2020
+
+class Exercise5_11 {
+	public static void main(String[] args) {
+		
+		int count = 0;
+		for (int i = 100; i <= 200; i++) {
+			
+			if (i%5 == 0 ^ i%6 == 0){
+				System.out.print(i + " " );
+				count++; 		
+			}	
+			if (count == 10){
+				System.out.println();
+				count = 0;
+			}
+		}
+		
+	}
+}
